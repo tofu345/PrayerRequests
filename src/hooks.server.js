f97289ee9fcd058613adaf9c823ab9f1e18b9a69
tofu345 <shammahayotunde1@gmail.com>
2024-09-22 04:25:44 +0100

    :|

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -16,16 +16,16 @@ export async function handle({ event, resolve }) {
     return response;
 }
 
-console.log("> init cron worker");
-CronJob.from({
-    cronTime: "* * * * *", // “At 13:30 on Sunday.”
-    // cronTime: "30 13 * * 0", // “At 13:30 on Sunday.”
-    onTick: async function () {
-        let deletedPosts = await deleteOldPosts();
-        if (deletedPosts.count > 0) {
-            console.log(`> Deleted ${deletedPosts.count} old posts`);
-        }
-    },
-    start: true,
-    timeZone: "Europe/London",
-});
+// console.log("> init cron worker");
+// CronJob.from({
+//     cronTime: "* * * * *", // “At 13:30 on Sunday.”
+//     // cronTime: "30 13 * * 0", // “At 13:30 on Sunday.”
+//     onTick: async function () {
+//         let deletedPosts = await deleteOldPosts();
+//         if (deletedPosts.count > 0) {
+//             console.log(`> Deleted ${deletedPosts.count} old posts`);
+//         }
+//     },
+//     start: true,
+//     timeZone: "Europe/London",
+// });
