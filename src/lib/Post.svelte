ccc6f16a6f32fd26ada022ff72872982813d04a9
tofu345 <shammahayotunde1@gmail.com>
2026-08-03 10:49:03 +0100

    minor: show month in post created date

--- src/lib/Post.svelte
+++ src/lib/Post.svelte
@@ -58,6 +58,6 @@ const imageClass = "p-[0.2rem] rounded";
         {/if}
     </div>
     <p class="text-xs text-gray-300 w-fit whitespace-pre-wrap">
-        {moment(post.createdAt).format("ddd Do")}
+        {moment(post.createdAt).format("ddd Do MMM")}
     </p>
 </div>
